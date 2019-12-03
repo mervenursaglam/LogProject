339af6087ea410778565b6b517e97f6f5bcc92a9
mervenursaglam <mervenursaglam@hotmail.com>
2019-12-03 22:57:00 +0300

    Yorum satirlari silindi.
    Yorum satirlari silindi.

--- LogApi-SPA/src/app/value/value.component.ts
+++ LogApi-SPA/src/app/value/value.component.ts
@@ -5,19 +5,14 @@ import {NgForm} from '@angular/forms';
 import {FormsModule} from '@angular/forms';
 import {PostService} from './../post.service';
 
-export interface value {
-  mes: string;
-}
-
 @Component({
   selector: 'app-value',
   templateUrl: './value.component.html',
   styleUrls: ['./value.component.css']
 })
+
 export class ValueComponent implements OnInit {
-  // postUrl: string ="http://localhost:53962/api/values";
-   //values:Value[]=[];
-   values:Value[];
+  values:Value[];
 
   httpOpsions={
     headers: new HttpHeaders({
@@ -34,12 +29,7 @@ export class ValueComponent implements OnInit {
   ngOnInit() {
    this.postService.GetAll()
    .subscribe((resp: Value[])=> this.values=resp);
-   // .subscribe({resp:Value[]} => this.values.resp);
-    //alert ("basladi");
-   // this.getValues().subscribe(data=>{
-     // this.values=data;
-   // })
-
+ 
   }
 
   Create(val: HTMLInputElement){
@@ -52,46 +42,16 @@ export class ValueComponent implements OnInit {
       this.values.push(resp);
     })
   }
- /*
-  postValues(val:HTMLInputElement){
-    const newPost: Post = {
-      body=val.value;
-    };
-    this.postS
-  }
-*/
-  getLog(){
-   // console.log(this.logmessage);
-   // alert(this.logmessage);
-  }
-    
-    
- /* onSubmit(){
-    return this.http.post("http://localhost:53962/api/values",).subscribe(data=> {
-      this.values=data;
-    });
-  }*/
-
+ 
   onNameKeyUp(event:any){
   
     this.logmessage=event.target.value;
   }
   
   postValues(val: Value ){
-
     var body = {
-
       de: this.logmessage,
-
+    }  
   }
-    
-    //const url="http://localhost:53962/api/values";
-   // return this.http.post(this.postUrl,val, this.httpOpsions)
-    
-  }
-    getValues(){
-    //return this.http.get<Value[]>("http://localhost:53962/api/values")
-  }
-
-
+ 
 }
